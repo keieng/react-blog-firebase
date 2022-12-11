206eafba52e7d3a635b6d1cdfca05be4c0a8a9ce
keieng <105902831+keieng@users.noreply.github.com>
2022-12-11 15:34:53 +0900

    投稿削除機能実装

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import { Button, Card, Container } from "react-bootstrap";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
+import { auth, db } from "../firebase";
+import { doc, deleteDoc } from "firebase/firestore";
 
 const Home = () => {
   const [postList, setPostList] = useState([]);
-  const deletePost = () => {};
+  const deletePost = async (id) => {
+    await deleteDoc(doc(db, "posts", id));
+    getPosts();
+  };
   const getPosts = async () => {
     const querySnapshot = await getDocs(collection(db, "posts"));
     const data = [];
     querySnapshot.forEach((doc) => {
       data.push({ id: doc.id, ...doc.data() });
     });
-    console.log(data);
     setPostList(data);
   };
   useEffect(() => {
@@ -26,21 +29,23 @@ const Home = () => {
       {postList.map((post) => {
         return (
           <Card className="mt-3" key={post.id}>
-            <Card.Header
-            /* className="bg-primary
-        text-white" */
-            >
-              {post.title}
-            </Card.Header>
+            <Card.Header>{post.title}</Card.Header>
             <Card.Body>{post.postText}</Card.Body>
             <Card.Footer className="text-end">
               <small className="text-muted">
                 @{post.author.username} Last updated 3 mins ago
               </small>
-              <Button variant="light" onClick={deletePost}>
-                <FontAwesomeIcon icon={faTrash} className="me-1" />
-                削除
-              </Button>
+              {post.author.id === auth.currentUser.uid && (
+                <Button
+                  variant="light"
+                  onClick={() => {
+                    deletePost(post.id);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faTrash} className="me-1" />
+                  削除
+                </Button>
+              )}
             </Card.Footer>
           </Card>
         );
